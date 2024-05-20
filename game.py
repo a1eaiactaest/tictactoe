2984584990fba3a3abc55bcb5c4d2843b2386474
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-05-20 18:41:20 +0200

    indent stuff

--- game.py
+++ game.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 import numpy as np
+
 from triplet import Board
 
-def random_game(state=None) -> None:
-  if state == None:
-    state = [0]*9+[1]
 
-  b = Board(state)
-  b.render_board()
-  print(b.is_game_over())
-  print(b.legal_moves())
-  done = False
-  while not done:
-    _, moves = b.legal_moves()
-    print(moves)
-    move = np.random.choice(1, moves)
-    print('\n', move)
-    s, (d, w) = b.make_move(move[0])
-    print(s,d,w)
-    done = d
+def random_game(state=None) -> None:
+    if state is None:
+        state = [0] * 9 + [1]
+
+    b = Board(state)
+    b.render_board()
+    print(b.is_game_over())
+    print(b.legal_moves())
+    done = False
+    while not done:
+        _, moves = b.legal_moves()
+        print(moves)
+        move = np.random.choice(1, moves)
+        print("\n", move)
+        s, (d, w) = b.make_move(move[0])
+        print(s, d, w)
+        done = d
+
 
 if __name__ == "__main__":
-  random_game()
+    random_game()
--- mcts.py
+++ mcts.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 import numpy as np
-from typing import Optional
 from state import State
 
+
 class Node:
-  def __init__(self, state: State):
-    self.state: State = state
-    self.parent: Node = None
-    self.children = {}
-    self.visits = 0
-    self.value = 0
+    def __init__(self, state: State, parent: "Node"):
+        self.state: State = state
+        self.parent: Node = parent
+        self.children = {}
+        self.visits = 0
+        self.value = 0
 
-  def is_leaf(self) -> bool:
-    return (len(self.children) == 0)
+    def is_leaf(self) -> bool:
+        return len(self.children) == 0
 
-  @property
-  def uct(self) -> float:
-    return self.value + 2 * np.sqrt(np.log(self.parent.visits) / self.visits)
+    @property
+    def uct(self) -> float:
+        return self.value + 2 * np.sqrt(np.log(self.parent.visits) / self.visits)
