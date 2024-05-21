0cdc981500b9ed132422f1af4e87b9534326bb14
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-05-21 22:54:30 +0200

    add timing helper

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import time
 import platform
 import contextlib
 from typing import Optional, Callable
@@ -14,3 +15,24 @@ def colored(st: str, color: Optional[str], background: bool = False) -> str:
         return f"\u001b[{10*background+60*(color.upper() == color)+30+['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'].index(color.lower())}m{st}\u001b[0m"
     else:
         return st
+
+
+class Timing(contextlib.ContextDecorator):
+    def __init__(
+        self, prefix: str = "", on_exit: Optional[Callable] = None, enabled: bool = True
+    ) -> None:
+        self.prefix = prefix
+        self.on_exit = on_exit
+        self.enabled = enabled
+
+    def __enter__(self) -> None:
+        self.st = time.perf_counter_ns()
+
+    def __exit__(self) -> None:
+        et = time.perf_counter_ns()
+        self.t = et - self.st
+        if self.enabled:
+            print(
+                f"{self.prefix}{self.t*1e-6:6.2} ms"
+                + (self.on_exit(self.t) if self.on_exit else "")
+            )
