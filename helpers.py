eaed6a17cce84eab598d7cf4ca384eee6e224333
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-05-21 22:52:10 +0200

    lint

--- helpers.py
+++ helpers.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import platform
-from typing import Optional
+import contextlib
+from typing import Optional, Callable
 
 
 PLAT = platform.system()
 assert PLAT == "Darwin" or PLAT == "Linux"
 
 
-def colored(st: str, color: Optional[str], background=False) -> str:
+def colored(st: str, color: Optional[str], background: bool = False) -> str:
     if color is not None:
         return f"\u001b[{10*background+60*(color.upper() == color)+30+['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'].index(color.lower())}m{st}\u001b[0m"
     else:
