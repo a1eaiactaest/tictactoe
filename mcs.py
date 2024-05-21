8a1dc98b61503b665e03fb703289f0bea6732a62
Piotrek Rybiec <piotrrybiec28@gmail.com>
2024-05-22 00:08:59 +0200

    profile mcs for optimization

--- mcs.py
+++ mcs.py
@@ -6,18 +6,19 @@ import matplotlib.pyplot as plt
 
 from state import State
 from mcts import MCTS
-
+from helpers import Timing, Profiling
 
 mmap = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
 N = int(1e5)
-D = 100
+D = 10
 initial_state = State([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 heatmap = np.zeros((9))
 
-for _ in trange(N):
-    M = MCTS(initial_state)
-    best = M.best_action(D)
-    heatmap[mmap.index(best)] += 1
+with Profiling():
+    for _ in trange(N):
+        M = MCTS(initial_state)
+        best = M.best_action(D)
+        heatmap[mmap.index(best)] += 1
 
 heatmap = heatmap.reshape((3, 3))
 print(heatmap)
